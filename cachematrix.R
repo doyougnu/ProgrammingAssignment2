f0a26bd648e332f2786ee9a038ba959704147c21
doyougnu <jmy6342@gmail.com>
2015-03-19 18:05:43 -0500

    Adding functions for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+#functions create special matrix wrapper that allows for caching the inverse
+#of the given matrix
 
-## Write a short comment describing this function
+#makeCacheMatrix accepts a matrix and returns a matrix of functions to get
+# or set the matrix to be operated on, or get or set the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  #function takes a matrix and caches the inversion
+  inv <- NULL
+  
+  setMat <- function(y){
+    myMatrix <<- y
+    inv <<- NULL
+  }
+  
+  getMat <- function() x
+  setInv <- function(matInv) inv <<- matInv
+  getInv <- function() inv
+  
+  return(matrix( data = list(setMat = setMat
+                             , getMat = getMat
+                             , setInv = setInv
+                             , getInv = getInv)
+                 , 2, 2))
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cacheSolve accepts the matrix of functions returned by makeCacheMatrix, checks
+#for the cached inverse, if found returns the inverse, if not the computes the inverse
+#and returns it
+cacheSolve <- function(x, ...){
+  
+  inv <- x[[2, 2]]()
+  
+  if (!is.null(inv)) {
+    message("retrieving cached inverse")
+    return(inv)
+  }
+  else {
+    
+    #copy the matrix, i think this is dangerous in real code as the functions
+    #or function indices may have changed
+    matCopy <- x[[2,1]]()
+    message("computing inverse")
+    
+    #this code should really be in setInv from the first function but doing it
+    #this way comports with the project requirements
+    retInv <- solve(matCopy)
+    x[[1, 2]](retInv)
+    return(retInv)
+  }
 }
